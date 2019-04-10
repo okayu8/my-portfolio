4cd1fba8b75cc1b97d4becf66378898f69c28deb
片岡祐太郎 <kataokayuutarou@kataokayuutarounoMacBook-Pro.local>
2019-04-11 01:33:56 +0900

    create two navItem

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import { spring, StaggeredMotion } from 'react-motion';
+import Demo from './Demo';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <Demo />
           <h1>
-            Yutaro Kataoka's<br />POIRTFOLIO
+            Yutaro Kataoka's<br />PORTFOLIO
           </h1>
+          {/* <StaggeredMotion
+            defaultStyles={[{ h: 0 }, { h: 0 }, { h: 0 }]}
+            styles={prevInterpolatedStyles => prevInterpolatedStyles.map((_, i) => {
+              return i === 0
+                ? { h: spring(100) }
+                : { h: spring(prevInterpolatedStyles[i - 1].h) }
+            })}>
+            {interpolatingStyles =>
+              <div>
+                {interpolatingStyles.map((style, i) =>
+                  <div key={i} style={{ border: '1px solid', height: style.h }} />)
+                }
+              </div>
+            }
+          </StaggeredMotion> */}
         </header>
       </div>
     );

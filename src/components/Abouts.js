99dcfc0b81227b6e969c94f13e8a2e992d08e83a
片岡祐太郎 <kataokayuutarou@kataokayuutarounoMacBook-Pro.local>
2019-04-20 12:37:41 +0900

    change about message

--- src/components/Abouts.js
+++ src/components/Abouts.js
@@ -52,8 +52,8 @@ export default class Abouts extends Component {
 
 const jp = {
     text1: "北海道出身のWebエンジニアです。",
-    text2: "PHP(Laravel, Phalcon)やASP.net Coreでのバックエンド開発を中心に、React.jsでのSPA開発やブロックチェーン(トークン)の開発などを行なっております。",
-    text3: "趣味でGo, Python, Rubyなどの言語を勉強しています。",
+    text2: "PHP(Laravel, Phalcon)やC#(ASP.net Core)でのバックエンド開発を中心に、React.jsでのフロントエンド開発やブロックチェーン(トークン)の開発なども行なっております。",
+    text3: "趣味でGo, Pythonなどの言語を勉強しています。",
     text4: "「開発効率はチームメンバーのモチベーション次第」と考えており、メンバーと気持ちよくコミュニケーションを取る事が大事だと考えます。",
     text5: "できるだけ見やすく、理解のしやすいコードを書く事を心がけています。",
     text6: "好きなエディターはVisualStudioCode, Vimです。",
@@ -61,7 +61,7 @@ const jp = {
 
 const en = {
     text1: "I am a web engineer from Hokkaido, Japan.",
-    text2: "I focus on backend development with PHP (Laravel, Phalcon) and ASP.net Core, and also do SPA development in React.js and development of blockchain (token).",
+    text2: "I focus on backend development with PHP (Laravel, Phalcon) and C#(ASP.net Core), and also do frontend development in React.js and development of BlockChain (token).",
     text3: "My hobbies are studying languages such as Go and Python.",
     text4: "I believe that \"development efficiency depends on the motivation of team members\" and I believe it is important to communicate with members comfortably.",
     text5: "I also try to write code that is as easy to read and understand as possible.",

105a65dff84c26c2e4cdc148d637ae398fabf3f9
片岡祐太郎 <kataokayuutarou@kataokayuutarounoMacBook-Pro.local>
2019-04-23 02:05:30 +0900

    Updates

---
+++ precache-manifest.5defb8261afd6c28dcc803a7f5990ffa.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "24b6d4cbdb455247f0d03a3f88ad8f39",
+    "url": "./static/media/icons8-azure.24b6d4cb.svg"
+  },
+  {
+    "revision": "05f2d8a380a711b45713",
+    "url": "./static/css/main.fc15a624.chunk.css"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "832cdbad31cda4698220427e47e2726e",
+    "url": "./static/media/icons8-java.832cdbad.svg"
+  },
+  {
+    "revision": "4272691fb8ab1e9669dc",
+    "url": "./static/js/2.f6801032.chunk.js"
+  },
+  {
+    "revision": "7b4f342847211296acd2eb313d30cd33",
+    "url": "./static/media/icons8-windows8-filled.7b4f3428.svg"
+  },
+  {
+    "revision": "744e4eb96630b7849686904a90eb9914",
+    "url": "./static/media/icons8-php-logo-filled.744e4eb9.svg"
+  },
+  {
+    "revision": "cab1a3b28e8a8bca5d017da91226c12d",
+    "url": "./static/media/icons8-c-sharp-logo-filled.cab1a3b2.svg"
+  },
+  {
+    "revision": "78bb185c7097a090d683df5b46b27a9d",
+    "url": "./static/media/prog-aspnet.78bb185c.svg"
+  },
+  {
+    "revision": "29247092d1d5fb6c42cf61682034c022",
+    "url": "./static/media/icons8-react-native.29247092.svg"
+  },
+  {
+    "revision": "20ebd1c792edccfe6dc2917c23795897",
+    "url": "./static/media/icons8-javascript-filled.20ebd1c7.svg"
+  },
+  {
+    "revision": "a29e71821c5768454c7d0cc921bce8d7",
+    "url": "./static/media/icons8-python-filled.a29e7182.svg"
+  },
+  {
+    "revision": "574b22fa556e5e06bf56a4142e0d5ede",
+    "url": "./static/media/icons8-golang-filled.574b22fa.svg"
+  },
+  {
+    "revision": "3f0daac17bbc6c2e0bed14d15a896ead",
+    "url": "./static/media/html5-01.3f0daac1.svg"
+  },
+  {
+    "revision": "7199fb6799b70651cefb2b09cad234c8",
+    "url": "./static/media/css3-01.7199fb67.svg"
+  },
+  {
+    "revision": "05f2d8a380a711b45713",
+    "url": "./static/js/main.f8880dc9.chunk.js"
+  },
+  {
+    "revision": "2ad2da63a064e64cd564dff15c939572",
+    "url": "./static/media/icons8-amazon-web-services.2ad2da63.svg"
+  },
+  {
+    "revision": "7bed95c0f044a3ba4a1281fb2a8efcb3",
+    "url": "./static/media/icons8-neo-cryptocurrency-filled.7bed95c0.svg"
+  },
+  {
+    "revision": "bd7a2c02c4fb1a2672579a9b54229016",
+    "url": "./static/media/icons8-git-filled.bd7a2c02.svg"
+  },
+  {
+    "revision": "58f660a6f949a26d3f644d7f24007655",
+    "url": "./static/media/icons8-docker-filled.58f660a6.svg"
+  },
+  {
+    "revision": "751ee1b97f5e218e96b1fb302738c216",
+    "url": "./static/media/icons8-slack-new.751ee1b9.svg"
+  },
+  {
+    "revision": "4587fa428092ff56f72e3f8206347631",
+    "url": "./static/media/icons8-trello-filled.4587fa42.svg"
+  },
+  {
+    "revision": "b97c205ac7615da2c443f0919e141fd2",
+    "url": "./static/media/icons8-webhook-filled.b97c205a.svg"
+  },
+  {
+    "revision": "8471acafe17de63f3a1222fea45c32cc",
+    "url": "./static/media/icons8-discord.8471acaf.svg"
+  },
+  {
+    "revision": "32d553b389ff9602bdf73f3602505fcd",
+    "url": "./static/media/os-apple-dark.32d553b3.svg"
+  },
+  {
+    "revision": "c75a40d8f12786331f8052a705519887",
+    "url": "./static/media/os-linux_1_.c75a40d8.svg"
+  },
+  {
+    "revision": "c4285dc6cc5116eb9dcc5b7d7f1e724a",
+    "url": "./static/media/my-portfolio.c4285dc6.png"
+  },
+  {
+    "revision": "91e4d5c1f8e740ab557581842ef3f87f",
+    "url": "./static/media/DSC01303.91e4d5c1.jpg"
+  },
+  {
+    "revision": "4272691fb8ab1e9669dc",
+    "url": "./static/css/2.6994900b.chunk.css"
+  },
+  {
+    "revision": "adc906d5794240683a50d7d04c862fd2",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/my-portfolio/precache-manifest.fe75aeebb6a57f43e35bf8b7987968bb.js"
+  "./precache-manifest.5defb8261afd6c28dcc803a7f5990ffa.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/my-portfolio/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

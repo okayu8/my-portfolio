6b47248d7de36dcd2686f3adefd673edb256330e
片岡祐太郎 <kataokayuutarou@kataokayuutarounoMacBook-Pro.local>
2019-04-22 22:55:34 +0900

    add skill icon

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -15,8 +15,6 @@ import cssicon from '../images/svgs/css3-01.svg';
 import azureicon from '../images/svgs/icons8-azure.svg';
 import awsicon from '../images/svgs/icons8-amazon-web-services.svg';
 import neoicon from '../images/svgs/icons8-neo-cryptocurrency-filled.svg';
-import mysqlicon from '../images/svgs/icons8-mysql-logo-filled.svg';
-// import sqlservericon from '../images/svgs/icons8-sql-240.svg';
 import winservericon from '../images/svgs/icons8-windows8-filled.svg';
 //tools
 import giticon from '../images/svgs/icons8-git-filled.svg';
@@ -28,6 +26,7 @@ import discordicon from '../images/svgs/icons8-discord.svg';
 //os
 import macicon from '../images/svgs/os-apple-dark.svg';
 import winicon from '../images/svgs/icons8-windows8-filled.svg';
+import linuxicon from '../images/svgs/os-linux_1_.svg';
 
 export default class Skills extends Component {
     render() {
@@ -44,13 +43,41 @@ export default class Skills extends Component {
                                 <a className="sub-title">Skills</a>
                             </h2>
                             <div className="content">
-                                <img className="skill-icon" src={azureicon} width="50" height="50"></img>
-                                <img src={azureicon} width="50" height="50"></img>
-                                <img src={azureicon} width="50" height="50"></img>
-                                <img src={azureicon} width="50" height="50"></img>
-                                <img src={azureicon} width="50" height="50"></img>
-                                <img src={azureicon} width="50" height="50"></img>
-                                <img src={azureicon} width="50" height="50"></img>
+                                <div className="sub-panel">
+                                    <h3 className="content-sub-title">Programming</h3>
+                                    <img className="skill-icon" src={phpicon} width="50" height="50"></img>
+                                    <img className="skill-icon" src={csicon} width="50" height="50"></img>
+                                    <img className="skill-icon" src={aspicon} width="50" height="50"></img>
+                                    <img className="skill-icon" src={reacticon} width="50" height="50"></img>
+                                    <img className="skill-icon" src={jsicon} width="50" height="50"></img>
+                                    <img className="skill-icon" src={pyicon} width="50" height="50"></img>
+                                    <img className="skill-icon" src={goicon} width="50" height="50"></img>
+                                    <img className="skill-icon" src={javaicon} width="50" height="50"></img>
+                                    <img className="skill-icon" src={htmlicon} width="50" height="50"></img>
+                                    <img className="skill-icon" src={cssicon} width="50" height="50"></img>
+                                </div>
+                                <div className="sub-panel">
+                                    <h3 className="content-sub-title">Infrastracture</h3>
+                                    <img className="skill-icon" src={azureicon} width="50" height="50"></img>
+                                    <img className="skill-icon" src={awsicon} width="50" height="50"></img>
+                                    <img className="skill-icon" src={winservericon} width="50" height="50"></img>
+                                    <img className="skill-icon" src={neoicon} width="50" height="50"></img>
+                                </div>
+                                <div className="sub-panel">
+                                    <h3 className="content-sub-title">Tools</h3>
+                                    <img className="skill-icon" src={giticon} width="50" height="50"></img>
+                                    <img className="skill-icon" src={dockericon} width="50" height="50"></img>
+                                    <img className="skill-icon" src={slackicon} width="50" height="50"></img>
+                                    <img className="skill-icon" src={trelloicon} width="50" height="50"></img>
+                                    <img className="skill-icon" src={webhookicon} width="50" height="50"></img>
+                                    <img className="skill-icon" src={discordicon} width="50" height="50"></img>
+                                </div>
+                                <div className="sub-panel">
+                                    <h3 className="content-sub-title">OS</h3>
+                                    <img className="skill-icon" src={macicon} width="50" height="50"></img>
+                                    <img className="skill-icon" src={winicon} width="50" height="50"></img>
+                                    <img className="skill-icon" src={linuxicon} width="50" height="50"></img>
+                                </div>
                             </div>
                         </div>
                     </div>
